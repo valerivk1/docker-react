be8276491bcf0e706e4b8dead9f7f0efeafcc048
root <root@ip-172-31-35-116.eu-central-1.compute.internal>
2022-07-21 13:14:26 +0000

    Travis CI 3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        	Alalalalalaa
+        	Alalalalala
 	  </p>
         <a
           className="App-link"
